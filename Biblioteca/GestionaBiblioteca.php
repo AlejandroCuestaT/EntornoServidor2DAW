a6ee363dcf5aecd22f9ebfe24b020131e6ac8fa1
AlejandroCuestaT <167741713+AlejandroCuestaT@users.noreply.github.com>
2026-02-24 13:59:34 +0100

    Update gestion

--- Biblioteca/GestionaBiblioteca.php
+++ Biblioteca/GestionaBiblioteca.php
@@ -11,17 +11,17 @@ require_once 'Biblioteca.php';
 $atenea = new Biblioteca();
 
 $l1 = new Libro("001", "Leyendas", 1850);
-$atenea->añadeDocumento($l1);
+$atenea->aniadeDocumento($l1);
 
 $l2 = new Libro("002", "El Quijote", 1590);
-$atenea->añadeDocumento($l2);
+$atenea->aniadeDocumento($l2);
 
 $r1 = new Revista("003", "National Geography");
-$atenea->añadeDocumento($r1);
+$atenea->aniadeDocumento($r1);
 
 $juan = new Socio("123456", "Juan");
-$atenea->añadeUsuario($juan);
-$atenea->añadeUsuario(new UsuarioOcasional("76434555", "Pedro"));
+$atenea->aniadeUsuario($juan);
+$atenea->aniadeUsuario(new UsuarioOcasional("76434555", "Pedro"));
 
 $atenea->devuelveDocumento($l1);
 $atenea->prestaDocumento($l1, $juan);
